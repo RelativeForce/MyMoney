fd00b3bbb81e6268e3f2f077c9b2fd6548a8b4f7
RelativeForce <joshiceddy@hotmail.com>
2020-07-09 18:11:51 +0100

    Removed files

--- MyMoney.Web/ClientApp/src/app/models/register.request.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RegisterRequest = /** @class */ (function () {
-    function RegisterRequest() {
-    }
-    return RegisterRequest;
-}());
-exports.RegisterRequest = RegisterRequest;
-//# sourceMappingURL=register.request.js.map
