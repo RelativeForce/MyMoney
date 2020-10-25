4579a4ff75e9a1696f5ea1ee95ebe0ac2a55d987
RelativeForce <joshiceddy@hotmail.com>
2020-10-25 11:10:25 +0000

    Removed MonthId

---
+++ MyMoney.Infrastructure/Migrations/20201025110827_RemoveMonthId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyMoney.Infrastructure.Migrations
+{
+   public partial class RemoveMonthId : Migration
+   {
+      protected override void Up(MigrationBuilder migrationBuilder)
+      {
+         migrationBuilder.DropColumn(
+             name: "MonthId",
+             table: "Budgets");
+      }
+
+      protected override void Down(MigrationBuilder migrationBuilder)
+      {
+         migrationBuilder.AddColumn<string>(
+             name: "MonthId",
+             table: "Budgets",
+             nullable: true);
+      }
+   }
+}
