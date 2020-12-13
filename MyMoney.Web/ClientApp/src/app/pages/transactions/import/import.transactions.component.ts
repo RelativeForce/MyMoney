86beb86a22519f944b6f07e12dd9d3d6d0ebbf74
RelativeForce <joshiceddy@hotmail.com>
2020-12-13 18:38:31 +0000

    Added parsing

--- MyMoney.Web/ClientApp/src/app/pages/transactions/import/import.transactions.component.ts
+++ MyMoney.Web/ClientApp/src/app/pages/transactions/import/import.transactions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { element } from 'protractor';
 import { BudgetService, IncomeService, TransactionService } from 'src/app/shared/services';
 
 @Component({
@@ -9,13 +10,17 @@ import { BudgetService, IncomeService, TransactionService } from 'src/app/shared
 export class ImportTransactionsComponent implements OnInit {
 
    public importTransactionsForm: FormGroup;
-   public rows: string[][];
+   public rows: Row[];
+   public headings: Heading[];
+   public fields: Field[];
 
    constructor(
       private readonly formBuilder: FormBuilder,
       private readonly transactionService: TransactionService,
    ) {
       this.rows = [];
+      this.headings = [];
+      this.fields = Object.keys(Field).map(k => Field[k] as Field);
    }
 
    public ngOnInit(): void {
@@ -38,16 +43,100 @@ export class ImportTransactionsComponent implements OnInit {
 
             const lines: string[] = text.split('\n');
 
+            let id = 0;
             for (const line of lines) {
-               this.rows[this.rows.length] = line.split(',').map(element => element.trim().replace(/"/g, ''));
+
+               const data = line.split(',').map(element => element.trim().replace(/"/g, ''));
+
+               this.rows[this.rows.length] = new Row(data, id++);
             }
 
+            this.resetHeadings();
          }
          reader.readAsText(input.files[index]);
       };
    }
 
-   public deleteRow(rowIndex: number) {
-      this.rows.splice(rowIndex, 1);
+   public resetHeadings(): void {
+      this.headings = [];
+
+      let columnCount = 0;
+      for (const row of this.rows) {
+         columnCount = Math.max(columnCount, row.data.length);
+      }
+
+      for (let index = 0; index < columnCount; index++) {
+         this.headings[index] = new Heading(Field.Description);
+      }
    }
+
+   public deleteRow(rowId: number): void {
+      this.rows = this.rows.filter(r => r.id !== rowId);
+   }
+
+   public changeElementValue(value: string, rowId: number, elementIndex: number) {
+      this.rows.find(r => r.id === rowId)?.setValue(elementIndex, value);
+   }
+
+   public onColumnChange(value: Field, elementIndex: number) {
+      this.headings[elementIndex].type = value;
+   }
+}
+
+class Row {
+   constructor(public data: string[], public id: number) {
+   }
+
+   setValue(index: number, value: string) {
+      this.data[index] = value;
+   }
+}
+
+class Heading {
+   constructor(public type: Field) { }
+
+   public get inputType() {
+      switch (this.type) {
+         case Field.Amount: return 'number';
+         case Field.Date: return 'date';
+         default: return 'text';
+      }
+   }
+
+   public get isIgnored() {
+      return this.type === Field.Ignore;
+   }
+
+   public format(input: string): any | null {
+      switch (this.type) {
+         case Field.Amount: return this.formatAmount(input);
+         case Field.Date: return this.formatDate(input);
+         default: return input;
+      }
+   }
+
+   private formatAmount(input: string): any {
+      const result = /-?[0-9][0-9,\.]+/g.exec(input) as RegExpExecArray | null;
+
+      if (result === null)
+         return null;
+
+      return Number.parseFloat(result[0]);
+   }
+
+   private formatDate(input: string): any {
+      try {
+         return new Date(Date.parse(input)).toISOString().split('T')[0];
+      } catch (error) {
+         return null;
+      }
+   }
+}
+
+enum Field {
+   Description = 'Description',
+   Notes = 'Notes',
+   Amount = 'Amount',
+   Date = 'Date',
+   Ignore = 'Ignore'
 }
