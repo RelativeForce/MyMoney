27318bd4721137e2bdc29ab3fd0bf7175630fc47
RelativeForce <joshiceddy@hotmail.com>
2020-12-25 20:51:13 +0000

    Updated migration to remove foreign keys aswell as indexs

--- MyMoney.Infrastructure/Migrations/20201215225806_UniqueIncomesAndBudgets.cs
+++ MyMoney.Infrastructure/Migrations/20201215225806_UniqueIncomesAndBudgets.cs
@@ -6,13 +6,21 @@ namespace MyMoney.Infrastructure.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incomes_Users_UserId",
+                table: "Incomes");
+
             migrationBuilder.DropIndex(
                 name: "IX_Incomes_UserId",
                 table: "Incomes");
 
-            migrationBuilder.DropIndex(
-                name: "IX_Budgets_UserId",
+            migrationBuilder.DropForeignKey(
+                name: "FK_Budgets_Users_UserId",
                 table: "Budgets");
+
+            migrationBuilder.DropIndex(
+                   name: "IX_Budgets_UserId",
+                   table: "Budgets");
 
             migrationBuilder.AlterColumn<string>(
                 name: "Name",
