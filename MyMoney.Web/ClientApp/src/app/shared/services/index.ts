4f6f938813fb7d6748fa0ebe47a8118c3c8a00b4
RelativeForce <joshiceddy@hotmail.com>
2021-01-24 11:50:17 +0000

    Fixed services

--- MyMoney.Web/ClientApp/src/app/shared/services/index.ts
+++ MyMoney.Web/ClientApp/src/app/shared/services/index.ts
@@ -1,6 +1,6 @@
-export * from './authentication-service.service';
-export * from './transaction-service.service';
-export * from './budget-service.service';
-export * from './income-service.service';
-export * from './current-user-service.service';
+export * from './authentication.service';
+export * from './transaction.service';
+export * from './budget.service';
+export * from './income.service';
+export * from './current-user.service';
 
