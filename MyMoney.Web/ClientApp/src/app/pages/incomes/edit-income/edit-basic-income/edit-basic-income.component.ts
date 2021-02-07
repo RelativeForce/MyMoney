02c10cce0a58ab3e5e66389a710d63dea8e6a7aa
RelativeForce <joshiceddy@hotmail.com>
2021-02-07 19:16:30 +0000

    Added link to parent

--- MyMoney.Web/ClientApp/src/app/pages/incomes/edit-income/edit-basic-income/edit-basic-income.component.ts
+++ MyMoney.Web/ClientApp/src/app/pages/incomes/edit-income/edit-basic-income/edit-basic-income.component.ts
@@ -4,10 +4,12 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { IncomeService } from '../../../../shared/services';
 import { BudgetViewModel } from '../../../../shared/classes';
 import { IIncomeModel } from 'src/app/shared/state/types';
-import { toInputDateString } from 'src/app/shared/functions';
+import { toFrequencyString, toInputDateString } from 'src/app/shared/functions';
+import { Frequency } from 'src/app/shared/api';
 
 @Component({
-   templateUrl: './edit-basic-income.component.html'
+   templateUrl: './edit-basic-income.component.html',
+   styleUrls: ['./edit-basic-income.component.scss']
 })
 export class EditBasicIncomeComponent implements OnInit {
 
@@ -15,8 +17,11 @@ export class EditBasicIncomeComponent implements OnInit {
    public selectedBudgets: Set<number> = new Set();
    public budgets: BudgetViewModel[] = [];
    public id: number;
+   public parentId: number | null = null;
+   public parentFrequency: Frequency | null = null;
    public loading = false;
    public submitted = false;
+   public loadingIncome = true;
 
    constructor(
       private readonly formBuilder: FormBuilder,
@@ -54,7 +59,11 @@ export class EditBasicIncomeComponent implements OnInit {
                this.f.name.patchValue(response.name);
                this.f.amount.patchValue(response.amount);
                this.f.notes.patchValue(response.notes);
-               this.enableForm();
+               this.parentId = response.parentId;
+               this.parentFrequency = response.parentFrequency;
+
+               this.loadingIncome = false;
+               this.enableForm(response.parentId !== null);
             },
                () => {
                   this.router.navigate(['/incomes']);
@@ -65,6 +74,14 @@ export class EditBasicIncomeComponent implements OnInit {
 
    public get f() {
       return this.editIncomeForm.controls;
+   }
+
+   public get frequencyString(): string {
+      if (this.parentFrequency === null) {
+         return '';
+      }
+
+      return `(${toFrequencyString(this.parentFrequency)})`;
    }
 
    public onSubmit(): void {
@@ -98,10 +115,13 @@ export class EditBasicIncomeComponent implements OnInit {
       this.f.notes.disable();
    }
 
-   private enableForm() {
-      this.f.amount.enable();
-      this.f.name.enable();
-      this.f.date.enable();
+   private enableForm(isChild: boolean) {
+      if (!isChild) {
+         this.f.date.enable();
+         this.f.name.enable();
+         this.f.amount.enable();
+      }
+
       this.f.notes.enable();
    }
 
