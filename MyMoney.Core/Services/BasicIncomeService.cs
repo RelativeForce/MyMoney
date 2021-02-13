82bcd4c55f825178be604f449662962f5b5d0386
RelativeForce <joshiceddy@hotmail.com>
2021-02-13 16:44:02 +0000

    Tweaked basic services

--- MyMoney.Core/Services/BasicIncomeService.cs
+++ MyMoney.Core/Services/BasicIncomeService.cs
@@ -35,12 +35,12 @@ namespace MyMoney.Core.Services
 
       public IIncome Add(DateTime date, string name, decimal amount, string notes)
       {
-         var user = _currentUserProvider.CurrentUser;
-
          if (string.IsNullOrWhiteSpace(name) || amount < 0.01m)
             return null;
 
          notes ??= string.Empty;
+
+         var user = _currentUserProvider.CurrentUser;
 
          var income = _entityFactory.NewIncome;
          income.UserId = user.Id;
@@ -91,6 +91,15 @@ namespace MyMoney.Core.Services
          var userId = _currentUserProvider.CurrentUserId;
 
          if (income == null || income.UserId != userId)
+            return false;
+
+         var basicDataHasChanged =
+            income.Amount != amount ||
+            income.Date != date ||
+            income.Name != name;
+
+         // Cannot edit the basic data of a recuring income child
+         if (income.ParentId != null && basicDataHasChanged)
             return false;
 
          income.Amount = amount;
