c0bf09b50fc84a83fdf7862dcb2432318b66e2c7
RelativeForce <joshiceddy@hotmail.com>
2021-03-06 00:28:00 +0000

    Added migration

--- MyMoney.Infrastructure/Entities/UserFeature.cs
+++ MyMoney.Infrastructure/Entities/UserFeature.cs
@@ -1,18 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyMoney.Core.Data;
 using MyMoney.Core.Interfaces.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyMoney.Infrastructure.Entities
 {
    public class UserFeature : IUserFeature
    {
       public long UserId { get; set; }
-      public IUser User { get; set; }
       public FeatureFlags Feature { get; set; }
+      [NotMapped]
+      public IUser User
+      {
+         get => UserProxy;
+         set => UserProxy = value as User;
+      }
+
+      [ForeignKey(nameof(UserId))]
+      public virtual User UserProxy { get; set; }
 
       internal static void Configure(ModelBuilder model)
       {
-         model.Entity<UserFeature>().HasIndex(uf => new { uf.UserId, uf.Feature }).IsUnique();
+         model.Entity<UserFeature>().HasKey(uf => new { uf.UserId, uf.Feature });
       }
    }
 }
---
+++ MyMoney.Infrastructure/Migrations/20210306002220_UserFeatures.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyMoney.Infrastructure.Migrations
+{
+   public partial class UserFeatures : Migration
+   {
+      protected override void Up(MigrationBuilder migrationBuilder)
+      {
+         migrationBuilder.CreateTable(
+             name: "UserFeature",
+             columns: table => new
+             {
+                UserId = table.Column<long>(nullable: false),
+                Feature = table.Column<int>(nullable: false)
+             },
+             constraints: table =>
+             {
+                table.PrimaryKey("PK_UserFeature", x => new { x.UserId, x.Feature });
+                table.ForeignKey(
+                       name: "FK_UserFeature_Users_UserId",
+                       column: x => x.UserId,
+                       principalTable: "Users",
+                       principalColumn: "Id",
+                       onDelete: ReferentialAction.Cascade);
+             });
+      }
+
+      protected override void Down(MigrationBuilder migrationBuilder)
+      {
+         migrationBuilder.DropTable(
+             name: "UserFeature");
+      }
+   }
+}
