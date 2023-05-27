7fff9f685f4b9acd653946380b79b23db70e25c0
Joshua Eddy <joshiceddy@outlook.com>
2023-05-27 23:57:15 +0100

    Moved session model

--- MyMoney.Web/FrontEnd/Angular/src/app/shared/state/types/index.ts
+++ MyMoney.Web/FrontEnd/Angular/src/app/shared/state/types/index.ts
@@ -1,4 +1,4 @@
-export * from './session-model.interface';
+export { ISessionModel } from 'mymoney-common/lib/interfaces';
 export * from './transaction-model.interface';
 export * from './date-range-model.interface';
 export * from './transactions-search-parameters.interface';
--- MyMoney.Web/FrontEnd/Common/src/interfaces/index.ts
+++ MyMoney.Web/FrontEnd/Common/src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './series.interface';
 export * from './series-item.interface';
 export * from './series-data-point.interface';
+export * from './session-model.interface';
