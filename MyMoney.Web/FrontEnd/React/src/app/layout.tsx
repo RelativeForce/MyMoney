bdfd5a87a5f497837657073aec85e7630a2baacb
Joshua Eddy <joshiceddy@outlook.com>
2023-05-28 00:40:24 +0100

    Moved layout to parent

--- MyMoney.Web/FrontEnd/React/src/app/layout.tsx
+++ MyMoney.Web/FrontEnd/React/src/app/layout.tsx
@@ -2,21 +2,24 @@
 
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import { useEffect } from "react";
+import { IUserDto } from 'mymoney-common/lib/api/dtos';
+import { useEffect, useState } from "react";
 
 export const metadata = {
   title: 'My Money - React',
   description: 'For all your budgeting needs',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children} : {children: React.ReactNode}) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
+
+  const [user, setUser] = useState<IUserDto | null>({
+    dateOfBirth: '',
+    fullName: 'Test user',
+    email: ''
+  });
 
   return (
     <html lang="en">
@@ -30,7 +33,55 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </head>
-      <body>{children}</body>
+      <body>
+      <div className="sticky-top">
+        {user !== null ? (
+          <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-between">
+            <a className="navbar-brand">MyMoney</a> {/* TODO: Redirect to home */}
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link">Home</a> {/* TODO: Redirect to home */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Transactions</a> {/* TODO: Redirect to transactions */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Budgets</a> {/* TODO: Redirect to budgets */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Incomes</a> {/* TODO: Redirect to incomes */}
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="userMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true"
+                  aria-expanded="false">
+                  {user.fullName}
+                </a>
+                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="userMenuLink">
+                  <a className="dropdown-item icon-dropdown-item"> {/* TODO: Redirect to user profile */}
+                    <span className="material-icons">person</span>Profile
+                  </a>
+                  <a className="dropdown-item icon-dropdown-item"> {/* TODO: Redirect to change password */}
+                    <span className="material-icons">vpn_key</span>Change password
+                  </a>
+                  <button className="dropdown-item icon-dropdown-item" > {/* TODO: Logout on click */}
+                    <span className="material-icons">logout</span>Logout
+                  </button>
+                </div>
+              </li>
+            </ul>
+          </nav>
+        ) : ''}
+      </div>
+      <div className="jumbotron">
+        <div className="container">
+          {/* TODO: Display children */}
+        </div>
+        {/* TODO: Footer component*/}
+      </div>
+        {children}
+        </body>
     </html>
   )
 }
--- MyMoney.Web/FrontEnd/React/src/app/page.tsx
+++ MyMoney.Web/FrontEnd/React/src/app/page.tsx
@@ -1,63 +1,7 @@
-'use client';
-
-import { useState } from 'react'
-import { IUserDto } from 'mymoney-common/lib/api/dtos';
-
-export default function Home() {
-  const [user, setUser] = useState<IUserDto | null>({
-    dateOfBirth: '',
-    fullName: 'Test user',
-    email: ''
-  });
-
+export default function Incomes() {
   return (
     <>
-      <div className="sticky-top">
-        {user !== null ? (
-          <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-between">
-            <a className="navbar-brand">MyMoney</a> {/* TODO: Redirect to home */}
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link">Home</a> {/* TODO: Redirect to home */}
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Transactions</a> {/* TODO: Redirect to transactions */}
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Budgets</a> {/* TODO: Redirect to budgets */}
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Incomes</a> {/* TODO: Redirect to incomes */}
-              </li>
-            </ul>
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="userMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true"
-                  aria-expanded="false">
-                  {user.fullName}
-                </a>
-                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="userMenuLink">
-                  <a className="dropdown-item icon-dropdown-item"> {/* TODO: Redirect to user profile */}
-                    <span className="material-icons">person</span>Profile
-                  </a>
-                  <a className="dropdown-item icon-dropdown-item"> {/* TODO: Redirect to change password */}
-                    <span className="material-icons">vpn_key</span>Change password
-                  </a>
-                  <button className="dropdown-item icon-dropdown-item" > {/* TODO: Logout on click */}
-                    <span className="material-icons">logout</span>Logout
-                  </button>
-                </div>
-              </li>
-            </ul>
-          </nav>
-        ) : ''}
-      </div>
-      <div className="jumbotron">
-        <div className="container">
-          {/* TODO: Display children */}
-        </div>
-        {/* TODO: Footer component*/}
-      </div>
+      Home
     </>
   )
 }
