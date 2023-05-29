cd6e8739dcc8d96efe15d89f9c37377d534589f9
Joshua Eddy <joshiceddy@outlook.com>
2023-05-29 12:10:10 +0100

    Added descruct support

--- MyMoney.Web/FrontEnd/React/src/hooks/user-session.ts
+++ MyMoney.Web/FrontEnd/React/src/hooks/user-session.ts
@@ -2,16 +2,16 @@ import { DependencyList, useEffect } from "react"
 import { selectCurrentSessionToken } from "@/state/session-slice";
 import { useSelector } from 'react-redux';
 
-export function useUserSession(operation: (sessionToken: string) => void, deps?: DependencyList) {
+export function useUserSession(operation: (sessionToken: string) => (void | (() => void)), deps?: DependencyList) {
     const sessionToken: string | null = useSelector(selectCurrentSessionToken);
 
     useEffect(
         () => {
-            if (!sessionToken){
+            if (!sessionToken) {
                 return; // Do nothing when no user is logged in
             }
-            
-            operation(sessionToken)
+
+            return operation(sessionToken)
         },
         [sessionToken, ...(deps ?? [])]
     );
